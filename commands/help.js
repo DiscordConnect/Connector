73eeca0c9dd244cd6fb3bc4b9b085fad89afe956
Cubity_First <cubity@cubityfir.st>
2018-10-17 13:53:40 +0100

    Show Prefix
    This avoids confusion between similar bots, that might use a @ command trigger.

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,8 @@
 exports.run = async (Discord, client, message, args) => {
   message.channel.send(`<@${message.author.id}>
-  \n**report** - Create a Report.
-  \n**edit** - Edit Reason / Proof of a Report.
-  \n**lookup** - Lookup a User's Basic Information, and any infractions.
-  \n**confirm** - Confirm an Existing Report.
-  \n**help** - This message, that you're reading, right now.`)
+  \n**${client.config.prefix}report** - Create a Report.
+  \n**${client.config.prefix}edit** - Edit Reason / Proof of a Report.
+  \n**${client.config.prefix}lookup** - Lookup a User's Basic Information, and any infractions.
+  \n**${client.config.prefix}confirm** - Confirm an Existing Report.
+  \n**${client.config.prefix}help** - This message, that you're reading, right now.`)
 };
