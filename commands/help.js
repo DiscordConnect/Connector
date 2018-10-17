1029583ff549e731c2ea425d7c46d3d2b0e88d05
Cubity_First <cubity@cubityfir.st>
2018-10-17 13:44:18 +0100

    Updated Help to be better
    Made it a bit easier on the eyes

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,8 @@
 exports.run = async (Discord, client, message, args) => {
   message.channel.send(`<@${message.author.id}>
-  \nreport - create a report
-  \nedit - edit the proof/reason of a report
-  \nlookup - lookup a user's profile data and infractions
-  \nconfirm - confirm an existing report
-  \nhelp - this message`)
+  \n**report** - Create a Report.
+  \n**edit** - Edit Reason / Proof of a Report.
+  \n**lookup** - Lookup a User's Basic Information, and any infractions.
+  \n**confirm** - Confirm an Existing Report.
+  \n**help** - This message, that you're reading, right now.`)
 };
